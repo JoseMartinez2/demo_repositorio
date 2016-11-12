3c6eba83ec8421043046bc78d13e977978f1fc1f
JoseMartinez2 <josemartinez19@ymail.com>
2016-11-12 07:01:49 -0600

    agregacion de valores
    se agregaron las variables y z

--- ClaseA.java
+++ ClaseA.java
@@ -1,8 +1,12 @@
 public class ClaseA{
 
-private int x;
-
-public claseA(){
-X=0;
+  private int x;
+  private int y;
+  private int z;  
+    public claseA(){
+       X = 0;
+       y = 0;
+       z = 0;
+  
 }
 }
